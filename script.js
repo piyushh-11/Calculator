8d8a6d0fe0eeb684c10971193ff0f1a7f89f3e24
piyushh-11 <115842755+piyushh-11@users.noreply.github.com>
2024-06-08 23:04:04 -0500

    fixed the input system

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let numOne = 0;
 let numTwo = 0;
 let operator = "";
 
+let operators = ["+", "-", "*", "/"];
+let hasOperator = false;
 let inputString = "";
 
 const buttons = document.querySelector(".buttons");
@@ -12,18 +14,38 @@ screen.textContent = 0;
 buttons.addEventListener("click", event => {
     if (event.target.nodeName == "BUTTON") {
         let curr = event.target.textContent
-        inputString += curr;
+        
 
         if (curr == "C") {
+            //inputString += curr;
             inputString = "0";
+            hasOperator = false;
         } else if (curr == "=") {
+            inputString += curr;
             evaluate(inputString)
             inputString = "";
+            hasOperator = false;
+        } else if (operators.includes(curr)) {
+            if (hasOperator) {
+                let chars = inputString.split("");
+                hasOperator = true;
+            } else {
+                inputString += curr;
+                hasOperator = true;
+            }
+        } else {
+            if (inputString == "0") {
+                inputString = curr;
+            } else {
+                inputString += curr;
+            }
         }
+
         screen.textContent = inputString;
     }
 });
 
-function evaluate(equation) {
-    alert(equation);
+function evaluate(numOne, numTwo, operator) {
+    console.log(numOne + operator + numTwo);
+    return "value";
 }
